b9f9dddbc1cb2e581d2510abb9364f251ce3c3a6
ThangnvPTIT <thangnv97ptit@gmail.com>
2018-10-23 23:33:54 +0700

    add socket handle

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const http = require('http')
+const socketIO = require('socket.io')
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
-console.log(publicPath)
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+io.on('connection', (socket) => {
+    console.log('new user connected ')
+    socket.on('disconnect', () => {
+        console.log('user was disconnected !!')
+    })
+})
+server.listen(port, () => console.log(`Server is running on port ${port}`));
